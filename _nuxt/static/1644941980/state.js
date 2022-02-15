599bfbc3b69ee9fe3aa317882ec03ace6fe2d102
Th1nkK1D <Th1nkK1D@users.noreply.github.com>
2022-02-15 16:19:41 +0000

    deploy: d3f2645e8c38cab2676fac2e49dda0d370d0b5f3

---
+++ _nuxt/static/1644941980/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpromise-tracker\u002F_nuxt\u002Fstatic\u002F1644941980",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpromise-tracker\u002F",assetsPath:"\u002Fpromise-tracker\u002F_nuxt\u002F",cdnURL:a}}}}(null));
