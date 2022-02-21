c67ecc08970f2ece3459533acb5de14646f9f126
Th1nkK1D <witheep@gmail.com>
2022-02-21 15:55:13 +0200

    Fix testing tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,7 @@ module.exports = {
     'prettier',
   ],
   plugins: [],
-  // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': [
-      'error',
-      {
-        ignores: ['default'],
-      },
-    ],
+    'vue/multi-word-component-names': 'off',
   },
 };
