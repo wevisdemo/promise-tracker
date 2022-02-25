3aaceb9af180657064afa49e0cbe9a6210cf4863
palminister <thanapatj.th@gmail.com>
2022-02-25 19:15:07 +0700

    Transform images to their path

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,8 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/fileTransformer.js',
   },
   collectCoverage: true,
   collectCoverageFrom: [
