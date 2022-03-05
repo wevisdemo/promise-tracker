d8979bd24a084403611b2e2e2f98b723b7ece033
palminister <thanapatj.th@gmail.com>
2022-03-05 21:46:15 +0700

    #20 Migrate status explanation component

--- tests/components/status-explaination.test.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import ProcessFlowchart from '@/components/process-flowchart.vue';
-import StatusExplaination from '@/components/status-explaination.vue';
-
-test('renders flowchart component', () => {
-  const wrapper = mount(StatusExplaination);
-  const flowchart = wrapper.getComponent(ProcessFlowchart);
-  expect(flowchart).toBeTruthy();
-});
