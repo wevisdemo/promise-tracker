34e917fe1eb38a5d1265e5673bc716c59c1db5e9
nathakits <nathakits@users.noreply.github.com>
2022-03-05 06:48:07 +0000

    deploy: c633d4510786bfc1c7ffc999b20102e59f37eb64

--- _nuxt/4b7a424.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{270:function(e,n,t){"use strict";t.r(n);var l=t(2).a.extend({name:"AboutPage"}),o=t(16),component=Object(o.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("About")])}),[],!1,null,null,null);n.default=component.exports}}]);
