52b8c5fbb74986988177589079f060edb24f3f04
Th1nkK1D <witheep@gmail.com>
2022-03-07 14:53:33 +0200

    #11 Add ChartItem

---
+++ components/explore/promise-overview/chart-item.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="flex flex-row text-white">
+    <div class="flex-1 flex flex-row items-center space-x-1">
+      <img
+        v-if="icon"
+        :src="`${$config.path.images}/${icon}`"
+        class="w-4 h-4"
+        :alt="label"
+      />
+      <p class="wv-u5 wv-font-semibold">{{ label }} ({{ count }})</p>
+    </div>
+    <div
+      class="flex flex-row flex-1 space-x-1 wv-u5 wv-font-semibold items-center"
+    >
+      <div
+        ref="barchart"
+        class="flex flex-row h-5"
+        :style="{ width: `${Math.round((count / max) * 100)}%` }"
+      >
+        <div
+          v-for="item in data"
+          :key="item.status"
+          :style="{ width: `${Math.round((item.count / count) * 100)}%` }"
+          :class="`bg-status-${item.status}`"
+        />
+      </div>
+      <span>{{ Math.round((count / total) * 100) }}%</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { PromiseStatus } from '~/models/promise';
+
+export default Vue.extend({
+  name: 'ChartItem',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: '',
+    },
+    data: {
+      type: Array as PropType<{ status: PromiseStatus; count: number }[]>,
+      required: true,
+    },
+    max: {
+      type: Number,
+      required: true,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    count() {
+      return this.data.reduce((sum, { count }) => sum + count, 0);
+    },
+  },
+});
+</script>
--- pages/explore.vue
+++ pages/explore.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-black min-h-screen">
     <h1>Explore</h1>
     <ActiveFilters
       :promises="promises"
@@ -20,6 +20,13 @@
     />
     <ActiveFilters :promises="promises" />
     <ActiveFilters />
+    <ChartItem
+      label="ไม่พบข้อมูล"
+      icon="status/nodata.png"
+      :data="items"
+      :max="15"
+      :total="60"
+    />
   </div>
 </template>
 
@@ -27,15 +34,25 @@
 import Vue from 'vue';
 import ActiveFilters from '@/components/explore/promise-overview/active-filters.vue';
 import promises from '@/data/promises-example.json';
+import ChartItem from '@/components/explore/promise-overview/chart-item.vue';
+import { PromiseStatus } from '@/models/promise';
 
 export default Vue.extend({
   name: 'ExplorePage',
   components: {
     ActiveFilters,
+    ChartItem,
   },
   data() {
     return {
       promises,
+      items: [
+        { status: PromiseStatus.NoData, count: 2 },
+        { status: PromiseStatus.Proposed, count: 3 },
+        { status: PromiseStatus.Paused, count: 1 },
+        { status: PromiseStatus.Working, count: 4 },
+        { status: PromiseStatus.Done, count: 5 },
+      ],
     };
   },
 });
