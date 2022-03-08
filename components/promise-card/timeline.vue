01ca8b8981614b1b293011f55d1cb2a46dd30f75
palminister <thanapatj.th@gmail.com>
2022-03-08 19:58:33 +0700

    #12 Add static timeline components

---
+++ components/promise-card/timeline.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col items-center">
+    <BoxContainer class="ultramarine-box">
+      <p class="wv-u5 wv-font-anuphan">3 เมษายน - 15 พฤษภาคม 2559</p>
+      <p class="wv-b5 wv-font-baijamjuri">เริ่มแจกบัตรสวัสดิการแห่งรัฐ</p>
+    </BoxContainer>
+    <TimelineArrow class="p-2" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BoxContainer from './box-container.vue';
+import TimelineArrow from './timeline-arrow.vue';
+
+export default Vue.extend({
+  name: 'Timeline',
+  components: { BoxContainer, TimelineArrow },
+});
+</script>
