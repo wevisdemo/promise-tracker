9c8017cde2baa35d6cf861903c31a5000e7c32df
palminister <thanapatj.th@gmail.com>
2022-03-10 21:23:49 +0700

    #12 Add link component

---
+++ components/promise-card/link.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="flex flex-col items-center">
+    <BoxContainer class="border-black border-2 wv-font-anuphan mb-3">
+      <a
+        :href="link.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="wv-font-anuphan wv-font-bold wv-u4 text-ultramarine underline"
+      >
+        {{ link.name }}
+      </a>
+    </BoxContainer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import BoxContainer from './box-container.vue';
+import { PromiseLink } from '@/models/promise';
+
+export default Vue.extend({
+  name: 'Link',
+  components: { BoxContainer },
+  props: {
+    link: {
+      type: Object as PropType<PromiseLink>,
+      default: () => ({
+        name: '',
+        url: '',
+      }),
+    },
+  },
+});
+</script>
