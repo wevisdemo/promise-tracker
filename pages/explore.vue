0dd2f6b74d1597c6135a27d83a49b108586b3a90
palminister <thanapatj.th@gmail.com>
2022-03-22 02:09:46 +0700

    #7 Clean up explore page

--- pages/explore.vue
+++ pages/explore.vue
@@ -4,24 +4,7 @@
     <div class="max-w-screen-lg mx-auto">
       <PromiseOverview :promises="promises" :filters="[]" />
     </div>
-    <div class="bg-gray">
-      <PromiseCard
-        v-for="promise in promises.slice(93, 96)"
-        :key="`promise-card-${promise.id}`"
-        class="pb-2"
-        :openState="false"
-        :promise="promise"
-      />
-    </div>
-    <div class="bg-black">
-      <PromiseCard
-        v-for="promise in promises.slice(96, 97)"
-        :key="`promise-card-${promise.id}`"
-        class="pb-2"
-        :openState="true"
-        :promise="promise"
-      />
-    </div>
+
     <div class="bg-status-done">
       <button class="border border-black p-2 mt-2" @click="groupBy = 'topic'">
         ตามประเด็น
@@ -31,6 +14,7 @@
       </button>
       {{ groupBy }}
     </div>
+
     <div v-if="groupBy === 'topic'" class="bg-gray">
       <div v-for="topic in topics" :key="`topic-${topic}`">
         <TopicGroup
@@ -66,7 +50,6 @@
 import Vue from 'vue';
 import PromiseOverview from '@/components/explore/promise-overview/promise-overview.vue';
 import promises from '@/data/promises.json';
-import PromiseCard from '@/components/promise-card/promise-card.vue';
 import TopicGroup from '@/components/explore/topic-group/topic-group.vue';
 import { PromiseTopic, PromiseStatus } from '@/models/promise';
 
@@ -74,7 +57,6 @@ export default Vue.extend({
   name: 'ExplorePage',
   components: {
     PromiseOverview,
-    PromiseCard,
     TopicGroup,
   },
   data() {
