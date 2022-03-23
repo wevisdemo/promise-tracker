932024713b2878097a570634a0b42a45b8e4178e
palminister <thanapatj.th@gmail.com>
2022-03-23 15:31:44 +0700

    #8 Update about page

--- pages/about.vue
+++ pages/about.vue
@@ -22,36 +22,6 @@
         </p>
       </WvParagraphGroup>
       <br /><br />
-      <WvParagraphGroup heading="ที่มาของข้อมูล">
-        <p>
-          ทางทีมผู้พัฒนา
-          ได้ใช้ข้อมูลจากเว็บไซต์จากเว็บไซต์ข่าวที่เขียนหรือกล่าวถึงนโยบาย
-          และบางคำสัญญาที่หาข้อมูลได้ จะสืบค้นข้อมูลต่อจากเอกสารทางราชการ เช่น
-          ราชกิจจานุเษกษา มติคณะรัฐมนตรี
-        </p>
-        <WvParagraphGroup :small="true" heading="หมายเหตุ">
-          <p>
-            พยายามรวบรวมข้อมูลให้ได้มากที่สุด โดยนำข้อมูลจากสำนักข่าวและเฟสบุ๊ค
-            เว็บไซต์ หรือทวิตเตอร์ ของพรรคการเมือง และข้อมูลรูปนโยบายบางพรรค
-            แต่บางส่วนไม่มีข้อมูลปรากฎแล้ว
-          </p>
-        </WvParagraphGroup>
-
-        <WvParagraphGroup :small="true" heading="อ้างอิงข้อมูลจาก :">
-          <ul class="list-disc list-outside ml-8">
-            <li
-              v-for="(reference, refIndex) in references"
-              :key="`reference-${refIndex}`"
-              class="hover:underline"
-            >
-              <ExternalLink class="break-all" :url="reference">{{
-                reference
-              }}</ExternalLink>
-            </li>
-          </ul>
-        </WvParagraphGroup>
-      </WvParagraphGroup>
-      <br /><br />
       <WvParagraphGroup heading="นโยบายการนำข้อมูลไปใช้ต่อ">
         <p>
           ทางทีมมีความตั้งใจที่พัฒนาทุกโปรเจ็กต์ให้เป็น Open Source
@@ -131,6 +101,20 @@
         target="_blank"
         rel="noopener noreferrer"
       />
+      <a
+        ref="feedback"
+        class="hidden"
+        :href="feedbackUrl"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
+      <a
+        ref="github"
+        class="hidden"
+        :href="githubUrl"
+        target="_blank"
+        rel="noopener noreferrer"
+      />
       <WvButton class="border-2" :onClick="handleDownloadLink"
         ><svg
           width="21"
@@ -182,8 +166,51 @@
         />
         <span>Feedback</span>
       </WvButton>
+      <WvButton class="border-2" :onClick="handleGithub">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+            fill="currentColor"
+          />
+        </svg>
+        <span>GitHub Repo</span>
+      </WvButton>
     </WvButtonGroup>
     <WvSharer :center="true" />
+    <WvParagraphGroup class="px-2 sm:px-0" heading="ที่มาของข้อมูล">
+      <p>
+        ทางทีมผู้พัฒนา
+        ได้ใช้ข้อมูลจากเว็บไซต์จากเว็บไซต์ข่าวที่เขียนหรือกล่าวถึงนโยบาย
+        และบางคำสัญญาที่หาข้อมูลได้ จะสืบค้นข้อมูลต่อจากเอกสารทางราชการ เช่น
+        ราชกิจจานุเษกษา มติคณะรัฐมนตรี
+      </p>
+      <WvParagraphGroup :small="true" heading="หมายเหตุ">
+        <p>
+          พยายามรวบรวมข้อมูลให้ได้มากที่สุด โดยนำข้อมูลจากสำนักข่าวและเฟสบุ๊ค
+          เว็บไซต์ หรือทวิตเตอร์ ของพรรคการเมือง และข้อมูลรูปนโยบายบางพรรค
+          แต่บางส่วนไม่มีข้อมูลปรากฎแล้ว
+        </p>
+      </WvParagraphGroup>
+
+      <WvParagraphGroup :small="true" heading="อ้างอิงข้อมูลจาก :">
+        <ul class="list-disc list-outside ml-8">
+          <li
+            v-for="(reference, refIndex) in references"
+            :key="`reference-${refIndex}`"
+            class="hover:underline"
+          >
+            <ExternalLink class="break-all" :url="reference">{{
+              reference
+            }}</ExternalLink>
+          </li>
+        </ul>
+      </WvParagraphGroup>
+    </WvParagraphGroup>
   </WvContainer>
 </template>
 
@@ -582,6 +609,7 @@ export default Vue.extend({
       downloadUrl:
         'https://docs.google.com/spreadsheets/d/1oXOgSQMrX3ZiUXxXMTZDglBVBH33BScFJAJr8gieYSk/edit?usp=sharing',
       feedbackUrl: 'https://airtable.com/shryu4errnlj1LWsM',
+      githubUrl: 'https://github.com/wevisdemo/promise-tracker',
     };
   },
   computed: {
@@ -604,6 +632,9 @@ export default Vue.extend({
     },
     handleFeedback() {
       (this.$refs.feedback as HTMLElement).click();
+    },
+    handleGithub() {
+      (this.$refs.github as HTMLElement).click();
     },
   },
 });
