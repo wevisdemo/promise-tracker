96dd32cc14265364fbb0aef7896139144406b3b0
mixth <me@mixth.net>
2022-03-23 23:18:13 +0700

    #15 Update fetch-data script and updated data

--- scripts/data-fetcher/index.ts
+++ scripts/data-fetcher/index.ts
@@ -5,19 +5,12 @@ import { getRawParties } from './extracts/party';
 import { transformToTrackingPromises } from './transforms/promise';
 import { transformToParties } from './transforms/party';
 
-const PROMISE_CSV =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vS3gvBNOgZnyEn75Emq-poR6laZrZ362qDLXmhlbYqniW4qQQk9LS8X76SNlNrGI5tSIjHCdYGXA16k/pub?gid=137904384&single=true&output=csv';
-const PROMISE_TIMELINE_CSV =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vS3gvBNOgZnyEn75Emq-poR6laZrZ362qDLXmhlbYqniW4qQQk9LS8X76SNlNrGI5tSIjHCdYGXA16k/pub?gid=27679393&single=true&output=csv';
-const PARTY_CSV =
-  'https://docs.google.com/spreadsheets/d/e/2PACX-1vS3gvBNOgZnyEn75Emq-poR6laZrZ362qDLXmhlbYqniW4qQQk9LS8X76SNlNrGI5tSIjHCdYGXA16k/pub?gid=1404897803&single=true&output=csv';
-
 async function fetchData() {
   const promises = await transformToTrackingPromises(
-    await getRawPromises(PROMISE_CSV),
-    await getRawPromiseTimelines(PROMISE_TIMELINE_CSV)
+    await getRawPromises(),
+    await getRawPromiseTimelines()
   );
-  const parties = await transformToParties(await getRawParties(PARTY_CSV));
+  const parties = await transformToParties(await getRawParties());
   await writeFile('./data/promises.json', JSON.stringify(promises, null, 2));
   await writeFile('./data/parties.json', JSON.stringify(parties, null, 2));
 }
