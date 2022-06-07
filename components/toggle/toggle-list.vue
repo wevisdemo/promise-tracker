498c40246592a2b799a3a5423545a49f3f107155
mixth <me@mixth.net>
2022-06-07 15:45:09 +0700

    #9 toggle-list component for filter-panel

---
+++ components/toggle/toggle-list.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-col space-y-1">
+    <ToggleItem
+      v-for="option in options"
+      :key="option.label"
+      :selected="option.label === selected"
+      :option="option"
+      @selected="handleSelected"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import ToggleItem from './toggle-item.vue';
+
+export interface ListOption {
+  label: string;
+  color?: string;
+}
+
+export default Vue.extend({
+  name: 'ToggleList',
+  components: { ToggleItem },
+  props: {
+    selected: {
+      type: String,
+      default: '',
+    },
+    options: {
+      type: Array as PropType<ListOption[]>,
+      default: () => [],
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    handleSelected(label: string) {
+      this.$emit('select', label);
+    },
+  },
+});
+</script>
